3d83d9fe07b5a14e9294cfdd675104d4c052af40
Diego Gomez <diegoalejogm@icloud.com>
2016-05-12 22:41:08 +0100

    Added test database data.

--- enHuecoAPIMain/settings.py
+++ enHuecoAPIMain/settings.py
@@ -85,11 +85,24 @@ WSGI_APPLICATION = 'enHuecoAPIMain.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
+"""
 DATABASES = {
+
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'enhueco',
+        'USER': 'enhuecouser',
+        'PASSWORD': 'ncodde123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
